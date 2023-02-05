9a72ad50e407f1e156a3a4b984e99be718de38cc
nir-itzhari <synexprox@gmail.com>
2023-02-05 16:38:54 +0200

    initail commit validate filter model

--- Backend/src/03-models/filter-model.ts
+++ Backend/src/03-models/filter-model.ts
@@ -8,22 +8,38 @@ export interface IFilterModel extends Document {
   fullName: string;
 }
 
-const filterSchema = new Schema({
+const filterSchema = new Schema<IFilterModel>({
   city: {
     type: String,
-    required: true
+    required: [true, "City is required"],
+    validate: {
+      validator: (city: string) => city.length >= 1 && city.length <= 50,
+      message: "City should be between 1 and 50 characters"
+    }
   },
   street: {
     type: String,
-    required: true
+    required: [true, "Street is required"],
+    validate: {
+      validator: (street: string) => street.length >= 1 && street.length <= 50,
+      message: "Street should be between 1 and 50 characters"
+    }
   },
   buildingNumber: {
     type: Number,
-    required: true
+    required: [true, "Building number is required"],
+    validate: {
+      validator: (buildingNumber: number) => Number.isInteger(buildingNumber),
+      message: "Building number must be a number"
+    }
   },
   fullName: {
     type: String,
-    required: true
+    required: [true, "Full name is required"],
+    validate: {
+      validator: (fullName: string) => fullName.length >= 1 && fullName.length <= 50,
+      message: "Full name should be between 1 and 50 characters"
+    }
   }
 });
 
