d401735f3bcfdf9a7693f3edef9b5b49c81e96a6
nir-itzhari <synexprox@gmail.com>
2023-02-06 03:17:18 +0200

    logic-controller for client first commit

---
+++ Backend/src/06-controllers/client-controller.ts
@@ -0,0 +1,53 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { ClientModel } from '../03-models/client-model';
+import clientLogic from '../05-logic/client-logic';
+
+const router = express.Router();
+
+router.get('/clients', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    try {
+        const assignments = await clientLogic.getAllClients();
+        response.status(200).json(assignments);
+    } catch (err: any) {
+        next(err);
+    }
+}
+);
+
+
+router.post('/client', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    try {
+        const client = new ClientModel(request.body);
+        const addedClient = await clientLogic.addClient(client);
+
+        response.status(201).json(addedClient);
+    } catch (err: any) {
+        next(err);
+    }
+}
+);
+
+router.put('/client', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    try {
+        const updatedClient = await clientLogic.updateClient(request.body);
+
+        response.status(200).json(updatedClient);
+    } catch (err: any) {
+        next(err);
+    }
+}
+);
+
+router.delete('/client/:clientId', async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { clientId } = request.params
+        await clientLogic.deleteClient(clientId);
+
+        response.sendStatus(204)
+    } catch (err: any) {
+        next(err);
+    }
+}
+);
+
+export default router;
