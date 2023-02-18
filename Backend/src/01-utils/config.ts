9e4c8bef4ed0d4975fb57140fb1431c3a840b1f6
nir-itzhari <synexprox@gmail.com>
2023-02-18 14:58:18 +0200

    setting up docker container for the back and db

--- Backend/src/01-utils/config.ts
+++ Backend/src/01-utils/config.ts
@@ -2,7 +2,7 @@ class Config { }
 
 class DevelopmentConfig extends Config {
     public isDevelopment = true;
-    public connectionString = "mongodb://localhost:27017/truegas"
+    public connectionString = "mongodb://db:27017/truegas"
 }
 
 class ProductionConfig extends Config {
